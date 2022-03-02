c0eaa4d71bca77953e870a14439b5b21f221b2aa
Will <wilito1210@gmail.com>
2022-03-01 23:59:05 -0500

    generate template function bug resolved && MVP done

--- src/generate-template.js
+++ src/generate-template.js
@@ -1,22 +1,62 @@
-const generateCard = function(employees) {
-    employees.forEach(element => {
-        `    
-        <div class="card text-white bg-primary mb-3 p-0" style="max-width: 18rem;">
-        <h5 class="card-title">${element.name}</h5>
-        <h6 class="card-subtitle mb-2 text-white">${element.role}</h6>
-        <div class="card-body bg-light text-dark">
-                <ul class="list-group list-group-flush bg-white">
-                    <li class="list-group-item">${element.id}</li>
-                    <li class="list-group-item">${element.email}</li>
-                    <li class="list-group-item">${element.officeNu}</li>
-                </ul>
-            </div>
-        </div>
-        `
-    });
-}
 
-const generateTemplate = function(employees) {
+const generateCards = function(array) {
+    let cards = []
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        console.log(element);
+        if (element.getRole() === 'Manager') {
+            cards.push(`    
+            <div class="card text-white bg-primary mb-3 p-0" style="max-width: 18rem;">
+            <h5 class="card-title">${element.getName()}</h5>
+            <h6 class="card-subtitle mb-2 text-white">${element.getRole()}</h6>
+            <div class="card-body bg-light text-dark">
+                    <ul class="list-group list-group-flush bg-white">
+                        <li class="list-group-item">${element.getId()}</li>
+                        <li class="list-group-item">${element.getEmail()}</li>
+                        <li class="list-group-item">${element.officeNumber}</li>
+                    </ul>
+                </div>
+            </div>
+        `)
+        } else if (element.getRole() === 'Engineer') {
+            cards.push(`    
+            <div class="card text-white bg-primary mb-3 p-0" style="max-width: 18rem;">
+            <h5 class="card-title">${element.getName()}</h5>
+            <h6 class="card-subtitle mb-2 text-white">${element.getRole()}</h6>
+            <div class="card-body bg-light text-dark">
+                    <ul class="list-group list-group-flush bg-white">
+                        <li class="list-group-item">${element.getId()}</li>
+                        <li class="list-group-item">${element.getEmail()}</li>
+                        <li class="list-group-item">${element.github}</li>
+                    </ul>
+                </div>
+            </div>
+        `)
+        } else if (element.getRole() === 'Intern') {
+            cards.push(`    
+            <div class="card text-white bg-primary mb-3 p-0" style="max-width: 18rem;">
+            <h5 class="card-title">${element.getName()}</h5>
+            <h6 class="card-subtitle mb-2 text-white">${element.getRole()}</h6>
+            <div class="card-body bg-light text-dark">
+                    <ul class="list-group list-group-flush bg-white">
+                        <li class="list-group-item">${element.getId()}</li>
+                        <li class="list-group-item">${element.getEmail()}</li>
+                        <li class="list-group-item">${element.school}</li>
+                    </ul>
+                </div>
+            </div>
+        `)
+        }      
+    }
+    console.log(cards.join("")); 
+    return `${cards.join("")}`
+    };
+
+
+    
+
+const generateTemplate = function(array) {
+    
     return `
 <!DOCTYPE html>
 <html lang="en">
@@ -29,9 +69,12 @@ const generateTemplate = function(employees) {
 </head>
 <body class="bg-primary">
     <div class="container d-flex justify-content-around">
-        ${generateCard(employees)}
+        ${generateCards(array)}
     </div>
 </body>
 </html>
 `
 }
+
+module.exports = generateTemplate
+
