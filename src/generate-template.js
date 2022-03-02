17d2031a56aacd24ae46e0c55edc6f776eeb2c92
Will <wilito1210@gmail.com>
2022-03-02 01:38:26 -0500

    ux improved and prompt validate functions added

--- src/generate-template.js
+++ src/generate-template.js
@@ -1,4 +1,4 @@
-
+//Generates one card(btrap component) for each member
 const generateCards = function(array) {
     let cards = []
     for (let index = 0; index < array.length; index++) {
@@ -7,12 +7,12 @@ const generateCards = function(array) {
         if (element.getRole() === 'Manager') {
             cards.push(`    
             <div class="card text-white bg-primary mb-3 p-0" style="max-width: 18rem;">
-            <h5 class="card-title">${element.getName()}</h5>
-            <h6 class="card-subtitle mb-2 text-white">${element.getRole()}</h6>
-            <div class="card-body bg-light text-dark">
+            <h5 class="card-title text-white text-center">${element.getName()}</h5>
+            <h6 class="card-subtitle mb-2 text-white text-center">${element.getRole()}</h6>
+            <div class="card-body bg-light text-dark text-center">
                     <ul class="list-group list-group-flush bg-white">
                         <li class="list-group-item">${element.getId()}</li>
-                        <li class="list-group-item">${element.getEmail()}</li>
+                        <li class="list-group-item"><a href="mailto:${element.getEmail()}">Email</a></li>
                         <li class="list-group-item">${element.officeNumber}</li>
                     </ul>
                 </div>
@@ -22,25 +22,25 @@ const generateCards = function(array) {
             cards.push(`    
             <div class="card text-white bg-primary mb-3 p-0" style="max-width: 18rem;">
             <h5 class="card-title">${element.getName()}</h5>
-            <h6 class="card-subtitle mb-2 text-white">${element.getRole()}</h6>
-            <div class="card-body bg-light text-dark">
+            <h6 class="card-subtitle mb-2 text-white text-center">${element.getRole()}</h6>
+            <div class="card-body bg-light text-dark text-center">
                     <ul class="list-group list-group-flush bg-white">
                         <li class="list-group-item">${element.getId()}</li>
-                        <li class="list-group-item">${element.getEmail()}</li>
-                        <li class="list-group-item">${element.github}</li>
+                        <li class="list-group-item"><a href="mailto:${element.getEmail()}">Email</a></li>
+                        <li class="list-group-item"><a href="https://github.com/${element.github}">GitHub</a></li>
                     </ul>
                 </div>
             </div>
         `)
         } else if (element.getRole() === 'Intern') {
             cards.push(`    
-            <div class="card text-white bg-primary mb-3 p-0" style="max-width: 18rem;">
+            <div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
             <h5 class="card-title">${element.getName()}</h5>
-            <h6 class="card-subtitle mb-2 text-white">${element.getRole()}</h6>
-            <div class="card-body bg-light text-dark">
+            <h6 class="card-subtitle mb-2 text-white text-center">${element.getRole()}</h6>
+            <div class="card-body bg-light text-dark text-center">
                     <ul class="list-group list-group-flush bg-white">
                         <li class="list-group-item">${element.getId()}</li>
-                        <li class="list-group-item">${element.getEmail()}</li>
+                        <li class="list-group-item"><a href="mailto:${element.getEmail()}">Email</a></li>
                         <li class="list-group-item">${element.school}</li>
                     </ul>
                 </div>
@@ -48,12 +48,8 @@ const generateCards = function(array) {
         `)
         }      
     }
-    console.log(cards.join("")); 
     return `${cards.join("")}`
     };
-
-
-    
 
 const generateTemplate = function(array) {
     
@@ -67,7 +63,7 @@ const generateTemplate = function(array) {
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Team Profile</title>
 </head>
-<body class="bg-primary">
+<body class="bg-secondary">
     <div class="container d-flex justify-content-around">
         ${generateCards(array)}
     </div>
